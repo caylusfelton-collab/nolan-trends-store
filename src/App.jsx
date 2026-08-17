fc4c40d34fa0dde34de3a1a748a51cf0655488f1
caylusfelton-collab <caylusfelton@gmail.com>
2026-08-17 09:58:26 +0300

    Restore original hero text

--- src/App.jsx
+++ src/App.jsx
@@ -502,7 +502,7 @@ function App() {
             </p>
 
             <h2>
-              WEAR YOUR STYLE
+              WEAR YOUR 
               <br />
               <strong>OWN STYLE.</strong>
             </h2>
